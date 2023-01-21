961bf6d7a6d987c4a6fcbbc86290b3b4e5886737
Tre <114157573+Tre-VN@users.noreply.github.com>
2023-01-21 22:20:56 +0700

    Delete Tre.Modules/Tre.Progress directory

--- Tre.Modules/Tre.Progress/Timer/Exit.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-export default async function (time) {
-    console.log('\x1b[32m' + 'Time spent: ' + time + 's' + '\x1b[0m');
-    console.log('\x1b[32m' + 'Press any key to exit this execution...' + '\x1b[0m');
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-    process.stdin.on('data', process.exit.bind(process, 0));
-};
