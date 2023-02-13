6e473c87b6e4ebfa70df0eec7d24c6eb352d4c3b
Haruma-VN <114157573+Tre-VN@users.noreply.github.com>
2023-02-13 17:36:27 +0700

    Add files via upload

--- Tre.Settings/UserControls/Element.xaml.cs
+++ Tre.Settings/UserControls/Element.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.IconPacks;
 
 namespace Tre.Settings.UserControls
 {
@@ -31,9 +19,9 @@ namespace Tre.Settings.UserControls
 
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Element));
 
-        public string Icon
+        public PackIconMaterialKind Icon
         {
-            get { return (string)GetValue(IconProperty); }
+            get { return (PackIconMaterialKind)GetValue(IconProperty); }
             set { SetValue(IconProperty, value); }
         }
         public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(Element));
