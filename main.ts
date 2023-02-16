8a232da4efd0c29947cc7fdba252f946e43c667c
Haruma-VN <114157573+Tre-VN@users.noreply.github.com>
2023-02-16 14:58:06 +0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 "use strict";
 import default_function from './Tre.Modules/Tre.Callback/default.js';
-async function Main(): Promise<void> {
-    await default_function().finally(() => { });
-};
-Main();
+export namespace Tre.Public {
+    export async function Main(): Promise<void> {
+        await default_function().finally(() => { });
+    };
+}
+Tre.Public.Main();
