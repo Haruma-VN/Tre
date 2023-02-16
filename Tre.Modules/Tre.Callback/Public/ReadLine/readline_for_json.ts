70c73b28734b526c92432aad2d4e0314e0e141ea
Haruma-VN <114157573+Tre-VN@users.noreply.github.com>
2023-02-16 15:46:56 +0700

    Add files via upload

--- Tre.Modules/Tre.Callback/Public/ReadLine/readline_for_json.ts
+++ Tre.Modules/Tre.Callback/Public/ReadLine/readline_for_json.ts
@@ -2,16 +2,17 @@
 import fs from "fs";
 import { Console } from "../../console.js";
 import { TreErrorMessage } from "../../../Tre.Debug/Tre.ErrorSystem.js";
+import localization from "../../localization.js";
 export default function processFilePaths(executor_file_need_avoid: string): string {
     let filePath =  Console.ReadLine();
     while (filePath !== '') {
         if (filePath === './') {
-            TreErrorMessage({ error: "Stopped", system: `"./" is not a valid file path.` }, `"./" is not a valid file path.`);
+            TreErrorMessage({ error: `${localization("stopped")}`, system: `"./" ${localization("not_a_valid_file_path")}` }, `"./" ${localization("not_a_valid_file_path")}`);
             filePath =  Console.ReadLine();
             continue;
         }
         if (filePath === executor_file_need_avoid) {
-            TreErrorMessage({ error: "Stopped", system: `Cannot apply to the patch file is using ${executor_file_need_avoid}` }, `Cannot apply to the patch file is using ${executor_file_need_avoid}`);
+            TreErrorMessage({ error: `${localization("stopped")}`, system: `${localization("cannot_apply_patch_file_is_using")} ${executor_file_need_avoid}` }, `${localization("cannot_apply_patch_file_is_using")} ${executor_file_need_avoid}`);
             filePath =  Console.ReadLine();
             continue;
         }
@@ -21,18 +22,18 @@ export default function processFilePaths(executor_file_need_avoid: string): stri
         try {
             const stats = fs.statSync(filePath);
             if (stats.isDirectory()) {
-                TreErrorMessage({ error: "Stopped", system: `${filePath} is a directory, not a file.` }, `${filePath} is a directory, not a file.`);
+                TreErrorMessage({ error: `${localization("stopped")}`, system: `${filePath} ${localization("is_a_directory_not_a_file")}` }, `${filePath} ${localization("is_a_directory_not_a_file")}`);
                 filePath =  Console.ReadLine();
                 continue;
             }
             if (!stats.isFile() || !filePath.endsWith('.json')) {
-                TreErrorMessage({ error: "Stopped", system: `${filePath} is not a valid .json file.` }, `${filePath} is not a valid .json file.`);
+                TreErrorMessage({ error: `${localization("stopped")}`, system: `${filePath} ${localization("is_a_directory_not_a_valid_json_file")}` }, `${filePath} ${localization("is_a_directory_not_a_valid_json_file")}`);
                 filePath =  Console.ReadLine();
                 continue;
             }
             break;
         } catch (err) {
-            TreErrorMessage({ error: "Stopped", system: `Error: ${filePath} is not a valid file path.` }, `Error: ${filePath} is not a valid file path.`);
+            TreErrorMessage({ error: `${localization("stopped")}`, system: `Error: ${filePath} ${localization("not_a_valid_file_path")}` }, `${filePath} ${localization("not_a_valid_file_path")}`);
             filePath =  Console.ReadLine();
         }
     }
