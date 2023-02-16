8208494e633fbb7ab3ef46a3d7a82aeb0e690b34
Haruma-VN <114157573+Tre-VN@users.noreply.github.com>
2023-02-16 15:46:41 +0700

    Add files via upload

--- Tre.Modules/Tre.Callback/Public/JSExecutor/ban.ts
+++ Tre.Modules/Tre.Callback/Public/JSExecutor/ban.ts
@@ -1,11 +1,17 @@
 "use strict";
 import { TreErrorMessage } from "../../../Tre.Debug/Tre.ErrorSystem.js";
-export default function (list_ban_function: (string)[], js_shell_string: string): boolean {
-    for (let ban_word of list_ban_function) {
-        if (js_shell_string.indexOf(ban_word) != -1) {
-            TreErrorMessage({ error: "Failed Execute JavaScript Void", reason: `${ban_word} is not defined` }, `${ban_word} is not defined`);
-            return false
+import localization from "../../localization.js";
+export default function (list_ban_function: string[], code: string): boolean {
+    const lines: string[] = code.split("\n");
+    for (const bannedWord of list_ban_function) {
+        const regex: RegExp = new RegExp(`\\b${bannedWord}\\.\\w+\\(`, "g");
+        for (const line of lines) {
+            let match;
+            while ((match = regex.exec(line))) {
+                TreErrorMessage({ error: `${localization("js_shell_execute_fail")}`, reason: `${bannedWord} ${localization("is_not_defined")}` }, `${bannedWord} ${localization("is_not_defined")}`);
+                return false;
+            }
         }
     }
-    return true
+    return true;
 }
