5ad7102ce9f09aabfcc577dd95698580b5a7e2b4
Haruma-VN <harumascaremath@gmail.com>
2023-03-13 18:29:12 +0700

    Create type.d.ts

---
+++ Tre.Modules/Tre.Libraries/Tre.FileSystem/type.d.ts
@@ -0,0 +1,44 @@
+declare type auto = any;
+
+declare type Void = void;
+
+declare type bool = boolean;
+
+declare type evaluate_width = number;
+
+declare type evaluate_height = number;
+
+declare type evaluate_percentages_number = number;
+
+declare type assertation_arg = string;
+
+declare type evaluate_error = NodeJS.ErrnoException;
+
+declare type auto_file_system_encoding = "hex" | "utf-8";
+
+declare type js_encode = "ascii" | "base64" | "base64url" | "utf-8" | "binary" | "hex" | "latin1" | "ucs-2" | "ucs2" | "utf16le" | "utf8";
+
+declare type str = string;
+
+declare type hash_method = "md5" | "sha1" | "sha256" | "sha512" | "ripemd160" | "whirlpool" | "sha3-224" | "sha3-256" | "sha3-384" | "sha3-512" | "blake2b" | "blake2s";
+
+declare type dimension_view = {
+    [x: string]: number;
+};
+
+declare type assertation_arguments = Array<string>;
+
+declare type arguments_asserations = Array<string>;
+
+declare type assertation_view = string;
+
+declare type debug_message = Array<auto>;
+
+declare type view_debug_text = string;
+
+declare type file_name = string;
+
+declare type file_save = file_name;
+
+declare type file_system_full_path_directory = file_name;
+
